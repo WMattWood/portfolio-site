2a7ae0d6c439f624aa93401ea78261fc4657d2cc
Matt Wood <w.matthew.wood@gmail.com>
2023-06-03 16:20:48 -0400

    Finished Navbar routing.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react"
 import styled from "styled-components"
+import {useNavigate} from 'react-router-dom'
+import Pdf from '../assets/Matthew_Wood_CV.pdf';
 
 const Navbar = () => {
 
+  const navigate = useNavigate()
+
   return (
     <Frame>
-        <Slink>About</Slink>
-        <Slink>My Projects</Slink>
-        <Slink>My Blog</Slink>
-        <Slink>Contact</Slink>
-        <Slink>CV</Slink>
+        <Link onClick={ () => navigate("/about")}>About</Link>
+        <Link onClick={ () => navigate("/projects")}>Projects</Link>
+        <Link onClick={ () => navigate("/blog")}>Blog</Link>
+        <Link onClick={ () => navigate("/contact")}>Contact</Link>
+        <FileLink href={Pdf}><Link>CV</Link></FileLink>
     </Frame>
   )
 }
@@ -21,8 +25,8 @@ const Frame = styled.div`
   margin-right: 5%;
 `
 
-const Slink = styled.h5`
-  
+const Link = styled.h5`
+  cursor: pointer;
   transition: 0.5s;
   margin: 12px;
   
@@ -30,5 +34,9 @@ const Slink = styled.h5`
     transition: 0.5s;
     color:red;
   }
+`
+
+const FileLink = styled.a`
+  text-decoration: none;
 `
 export default Navbar
